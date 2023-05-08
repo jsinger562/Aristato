5e5ef074871dca530ff5621bdd7cc7e15ea9fc9a
jsinger562 <71184946+jsinger562@users.noreply.github.com>
2023-05-08 11:03:29 -0400

    Update pre-processing.py

--- data/pre-processing.py
+++ data/pre-processing.py
@@ -1,7 +1,5 @@
 import re
 import numpy as np
-import torch
-from transformers import BertTokenizer, BertForSequenceClassification
 
 def break_into_chunks(text, length_of_chunks):
     # helper method that breaks string text into a list of sentences
@@ -70,50 +68,3 @@ for sentence in list_of_sentences:
     file6.write(sentence + '\n')
 file6.close()
 file5.close()
-
-list_of_sentences = ["this is the first sentence", "this is the second sentence"]
-tensor = torch.tensor([sentence.split() for sentence in list_of_sentences])
-print(tensor.shape)
-
-"""
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = torch.hub.load('huggingface/pytorch-transformers', 'model', 'bert-base-uncased') 
-indexed_tokens = tokenizer.encode(sentences[0], add_special_tokens=True)
-segments_ids = [0 for _ in range(len(indexed_tokens))]
-segments_tensors = torch.tensor([segments_ids])
-tokens_tensor = torch.tensor([indexed_tokens])
-with torch.no_grad():
-    encoded_layers, _ = model(tokens_tensor, token_type_ids=segments_tensors)
-#print(encoded_layers.shape)
-embedding = torch.mean(encoded_layers, dim=1).squeeze()
-
-print(embedding)
-
-
-tokens = tokenizer.tokenize(sentences[0])
-tokens = ['[CLS]'] + tokens + ['[SEP]']
-T = 20
-padded_tokens=tokens +['[PAD]' for _ in range(T-len(tokens))]
-attn_mask=[1 if token != '[PAD]' else 0 for token in padded_tokens]
-seg_ids=[0 for _ in range(len(padded_tokens))]
-print("Segment Tokens are \n {}".format(seg_ids))
-sent_ids=tokenizer.convert_tokens_to_ids(padded_tokens)
-print("senetence idexes \n {} ".format(sent_ids))
-token_ids = torch.tensor(sent_ids).unsqueeze(0) 
-attn_mask = torch.tensor(attn_mask).unsqueeze(0) 
-seg_ids   = torch.tensor(seg_ids).unsqueeze(0)
-print(tokens)
-
-hidden_reps, cls_head = bert_model(token_ids, attention_mask = attn_mask,token_type_ids = seg_ids)
-print(hidden_reps)
-print(cls_head)
-"""
-
-"""
-s_list = s.split()
-s_lst_np = np.asanyarray(s_list)
-print(s_lst_np)
-numbers = np.zeros(100)
-
-
-"""
